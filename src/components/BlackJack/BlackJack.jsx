08fe87e99365ab3d6e4eac96918b0783509e76b5
Eddy Soma <esoma92.dev@gmail.com>
2020-05-23 18:38:37 -0500

    add blackjack game states

--- src/components/BlackJack/BlackJack.jsx
+++ src/components/BlackJack/BlackJack.jsx
@@ -3,13 +3,54 @@ import './BlackJack.css';
 import { Link } from 'react-router-dom';
 
 export class BlackJack extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inGame: false,
+      playerCash: 1000
+    };
+  }
+
   render() {
+    let screen;
+    if (this.state.inGame) {
+      screen = (
+        <div>
+          <h2>Playing Black Jack</h2>
+          <h2>Your cash: ${this.state.playerCash}</h2>
+          <button type="button" onClick={this.end}>End Game</button>
+        </div>
+      )
+    } else {
+      screen = (
+        <div>
+          <h2>Welcome to Black Jack</h2>
+          <button type="button" onClick={this.start}>Start Game</button>
+          <Link to="/">Quit</Link>
+        </div>
+      )
+    }
+
     return (
       <div className="BlackJack">
         <h1>BlackJack reacts!</h1>
-        <Link to="/">Back</Link>
+        <h2>Black Jack Rules.</h2>
+        {screen}
       </div>
     );
+  }
+
+  start = () => {
+    this.setState({
+      inGame: true
+    });
+  }
+
+  end = () => {
+    this.setState({
+      playerCash: 1000,
+      inGame: false
+    });
   }
 };
 
