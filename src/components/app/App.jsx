393b022efaf2cda48c4b26d837830092f5a37cd0
Eddy Soma <esoma92.dev@gmail.com>
2020-08-05 23:52:11 -0500

    add styles to the landing page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,19 +5,27 @@ import { BlackJack } from '../BlackJack';
 import { Nim } from '../Nim';
 import { TechTalk } from '../TechTalk';
 
+// Asset import
+import bjIcon from '../../assets/blackjack.svg';
+import nimIcon from '../../assets/nim.svg';
+import ghIcon from '../../assets/github.svg';
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Tri Land reacts!</h1>
-        <h1>Tri Header</h1>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/blackjack' component={BlackJack} />
           <Route path='/nim' component={Nim} />
           <Route path='/tecktalk' component={TechTalk} />
         </Switch>
-        <h1>Tri Footer</h1>
+        <div id="footer">
+          <div id="ft-box">
+            <a href="https://github.com/Kairn/tri-land" target="_blank" rel="noopener noreferrer"><img src={ghIcon} alt="github" /></a>
+            <i>Designed by: Eddy Soma</i>
+          </div>
+        </div>
       </div>
     </Router>
   );
@@ -26,18 +34,27 @@ function App() {
 function Home() {
   return (
     <div className="Home">
-      <h1>Tri Home</h1>
-      <ul>
-        <li>
-          <Link to="/blackjack">Black Jack</Link>
-        </li>
-        <li>
-          <Link to="/nim">Nim Game</Link>
-        </li>
-        <li>
-          <Link to="/tecktalk">Tech Talk</Link>
-        </li>
-      </ul>
+      <div id="title" className="font-coug">
+        <h1 id="tl-title">Tri Land</h1>
+        <h1>A Reactive Experience</h1>
+      </div>
+      <div id="home-main">
+        <div id="fb-1" className="feature-box">
+          <h2 className="feature-title">Blackjack</h2>
+          <img className="feature-icon" src={bjIcon} alt="blackjack" />
+          <p>Feeling lucky? Let's see how much can you win!</p>
+          <Link className="feature-btn" to="/blackjack">Play</Link>
+        </div>
+        <div id="fb-2" className="feature-box">
+          <h2 className="feature-title">Nim Game</h2>
+          <img className="feature-icon" src={nimIcon} alt="nim" />
+          <p>Feeling smart? Let's see how quickly can you think!</p>
+          <Link className="feature-btn" to="/nim">Play</Link>
+        </div>
+      </div>
+      <div id="feedback">
+        <Link id="tt-btn" to="/tecktalk">Tech Talk</Link>
+      </div>
     </div>
   );
 }
